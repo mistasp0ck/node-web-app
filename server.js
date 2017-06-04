ef0d63aa661b9d959e0f9005f2f9f89913783bbd
tones <tones@fredericks-MacBook-Pro-2.local>
2017-06-04 08:20:50 -0400

    Created projects page and added link to header partial

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Portfolio projects page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
